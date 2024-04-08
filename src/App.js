1865a456ed56c9c8f0b933d59cac099f18d1e4e5
PSouza <psouza@wtecenergy.com>
2024-04-08 11:51:32 -0300

    Initial template - Add step numbers and messages to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+const messages = ["Learn to test ⚛️", "Start Testing 💼", "Do it again 🧪"];
+
 export default function App() {
+  const step = 1;
+
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
